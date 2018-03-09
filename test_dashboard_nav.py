02dcfb7605fa238dd6bae1e52aa041c42c341140
Thomas Kluyver <thomas@kluyver.me.uk>
2018-03-09 18:34:55 +0000

    Current state of test changes (not working)

--- test_dashboard_nav.py
+++ test_dashboard_nav.py
@@ -10,6 +10,7 @@ def get_list_items(browser):
     return [{
         'link': a.get_attribute('href'),
         'label': a.find_element_by_class_name('item_name').text,
+        'element': a,
     } for a in browser.find_elements_by_class_name('item_link')]
 
 
@@ -17,7 +18,7 @@ def wait_for_selector(browser, selector, timeout=10):
     wait = WebDriverWait(browser, timeout)
     return wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
 
-
+import time
 
 def test_items(authenticated_browser, visited=None):
     tree_root_url = authenticated_browser.current_url
@@ -27,6 +28,19 @@ def test_items(authenticated_browser, visited=None):
     wait_for_selector(authenticated_browser, '.item_link')
     items = get_list_items(authenticated_browser)
     print(authenticated_browser.current_url, len(items))
+
+    # steps_back = 0
+    # while True:
+    #     find items
+    #     down a level
+    #     steps_back += 1
+    #     back
+    #     if no folders:
+    #         pass
+    #
+    # for i in range(steps_back):
+    #     back
+
     for item in items:
         print(item)
         url = item['link']
@@ -35,11 +49,18 @@ def test_items(authenticated_browser, visited=None):
             if url in visited:
                 continue
             visited.add(url)
-            authenticated_browser.get(url)
+            #authenticated_browser.get(url)
+            item['element'].click()
             wait_for_selector(authenticated_browser, '.item_link')
             assert authenticated_browser.current_url == url
 
+
+            time.sleep(2)
+
             test_items(authenticated_browser, visited)
-            #browser.back()
+            authenticated_browser.back()
+            wait_for_selector(authenticated_browser, '.item_link')
+
+            time.sleep(1)
 
     print()
